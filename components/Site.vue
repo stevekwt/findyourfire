4b7c8045d767885a5a7cc61a2e91d1dd47524e06
Stephen Thomas <66618292+stevekwt@users.noreply.github.com>
2022-01-19 18:10:06 -0800

    second commit

---
+++ components/Site.vue
@@ -0,0 +1,12 @@
+
+<template>
+    <div class="container">
+        <Hero/>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtSite'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <Site/>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
