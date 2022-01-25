38dd8903ded041b948ebb416bd34d7e623b1b66a
Stephen Thomas <66618292+stevekwt@users.noreply.github.com>
2022-01-25 11:05:45 -0800

    Meta data updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,14 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'client',
+    title: 'Find your Fiery Foundlings Who Fight Fire With Fire',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'fire finding' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,20 @@
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  head: {
+    title: 'Fiery Foundlings Fight Fire With Fire',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Fiery foundlings will find your fire fighting and fight fire with fire'
+      }
+    ]
+  }
+  // metaInfo: {
+  //   title: 'Default Title for Fire Finding',
+  //   titleTemplate: '%s | Fire Finding'
+  // }
 }
 </script>
