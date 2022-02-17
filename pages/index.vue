d2f4c3fff658cee6a4dbcae9ee854fd81c461262
Stephen Thomas <66618292+stevekwt@users.noreply.github.com>
2022-02-16 21:39:27 -0800

    basic frontpage up w images

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,39 @@
 <template>
-  <Site/>
+   <div class="container">
+        <Hero />
+        <LargeCardDisplay
+          v-for="cardInfo in largeCardInfo"
+          :key="cardInfo.id"
+          :cardsSection="cardInfo" 
+        />
+    </div>
 </template>
 
 <script>
+// import LargeCardDisplay from "../components/LargeCardDisplay.vue";
+import { largeCardSections } from "@/assets/data.js"
 export default {
-  name: 'IndexPage',
-  head: {
-    title: 'Fiery Foundlings Fight Fire With Fire',
-    meta: [
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'Fiery foundlings will find your fire fighting and fight fire with fire'
-      }
-    ]
+  data() {
+    return {
+      largeCardInfo: largeCardSections
+    }
   }
-  // metaInfo: {
-  //   title: 'Default Title for Fire Finding',
-  //   titleTemplate: '%s | Fire Finding'
-  // }
+    // name: "IndexPage",
+    // head: {
+    //     title: "Fiery Foundlings Fight Fire With Fire",
+    //     meta: [
+    //         {
+    //             hid: "description",
+    //             name: "description",
+    //             content: "Fiery foundlings will find your fire fighting and fight fire with fire"
+    //         }
+    //     ]
+    // }
+    // // metaInfo: {
+    // //   title: 'Default Title for Fire Finding',
+    // //   titleTemplate: '%s | Fire Finding'
+    // // }
+    // ,
+    // components: { LargeCardDisplay }
 }
 </script>
